c90357220f4bb8c17ceedcd6928c366285bac548
JeyashreeGWL <jeyashree.r@galaxyweblinks.in>
2026-07-06 16:39:36 +0530

    customer data fix

--- extensions/loyalty-checkout/shopify.d.ts
+++ extensions/loyalty-checkout/shopify.d.ts
@@ -2,18 +2,18 @@ import '@shopify/ui-extensions';
 
 //@ts-ignore
 declare module './src/Checkout.jsx' {
-  const shopify: import('@shopify/ui-extensions/purchase.checkout.block.render').Api;
+  const shopify: import('@shopify/ui-extensions/purchase.checkout.contact.render-after').Api;
   const globalThis: { shopify: typeof shopify };
 }
 
 //@ts-ignore
 declare module './src/api.js' {
-  const shopify: import('@shopify/ui-extensions/purchase.checkout.block.render').Api;
+  const shopify: import('@shopify/ui-extensions/purchase.checkout.contact.render-after').Api;
   const globalThis: { shopify: typeof shopify };
 }
 
 //@ts-ignore
 declare module './src/api-base-url.js' {
-  const shopify: import('@shopify/ui-extensions/purchase.checkout.block.render').Api;
+  const shopify: import('@shopify/ui-extensions/purchase.checkout.contact.render-after').Api;
   const globalThis: { shopify: typeof shopify };
 }
